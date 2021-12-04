ce3b5afdbdab0c6b682660744c17425651efb9d0
Krishang Patney <krishangpatney@icloud.com>
2021-12-04 09:34:50 -0800

    Metrics work - unfinished viz added

--- src/Part_1/pineapple/get_metrics.sh
+++ src/Part_1/pineapple/get_metrics.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# Get's metric values and stores them in output/raw_metrics.
- 
+# ./get_metrics.sh krishangs_resource pineapplication-site
 
+resource_group=${1}
+application_name=${2} 
+
+# Get's metric values and stores them in output/raw_metrics.
+echo $resource_group
+echo $application_name
+
+# Percentage CPU
+az vm monitor metrics tail --name $application_name -g $resource_group --metric "Percentage CPU" > 'output/raw_metrics/percentageCPU.json'
+
+# Available Memory Bytes
+az vm monitor metrics tail --name $application_name -g $resource_group --metric "Available Memory Bytes" > 'output/raw_metrics/availableMemoryBytes.json'
